e63b54a1dbc75815c77424135bad3d4b309dd1eb
James <jtclarkjr@ucla.edu>
2025-03-25 01:06:00 +0900

    Update transit.go

--- controllers/transit.go
+++ controllers/transit.go
@@ -20,9 +20,11 @@ func fetchNodes(station string, channel chan<- string, wg *sync.WaitGroup) {
 	host := os.Getenv("RAPIDAPI_TRANSPORT_HOST")
 
 	url := fmt.Sprintf("https://%s/transport_node?word=%s&limit=1", host, station)
+	log.Printf("Fetching node for station: %s, URL: %s", station, url)
 
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		log.Printf("Error creating request for station %s: %v", station, err)
 		channel <- ""
 		return
 	}
@@ -32,6 +34,7 @@ func fetchNodes(station string, channel chan<- string, wg *sync.WaitGroup) {
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
+		log.Printf("Error fetching data for station %s: %v", station, err)
 		channel <- ""
 		return
 	}
@@ -39,34 +42,42 @@ func fetchNodes(station string, channel chan<- string, wg *sync.WaitGroup) {
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
+		log.Printf("Error reading response for station %s: %v", station, err)
 		channel <- ""
 		return
 	}
 
+	log.Printf("Response body for station %s: %s", station, string(body))
+
 	var data map[string]interface{}
 	if err := json.Unmarshal(body, &data); err != nil {
+		log.Printf("Error unmarshaling response for station %s: %v", station, err)
 		channel <- ""
 		return
 	}
 
 	items, found := data["items"].([]interface{})
 	if !found || len(items) == 0 {
+		log.Printf("No items found in response for station %s. Full response: %s", station, string(body))
 		channel <- ""
 		return
 	}
 
 	itemMap, ok := items[0].(map[string]interface{})
 	if !ok {
+		log.Printf("Error processing item map for station %s", station)
 		channel <- ""
 		return
 	}
 
 	nodeId, ok := itemMap["id"].(string)
 	if !ok {
+		log.Printf("No node ID found for station %s", station)
 		channel <- ""
 		return
 	}
 
+	log.Printf("Found node ID for station %s: %s", station, nodeId)
 	channel <- nodeId
 }
 
