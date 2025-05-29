7b038a4d550ab65a7d7bc1bc2bcc841ec6c3b026
J <jtclarkjr@ucla.edu>
2025-05-29 13:50:02 +0900

    added middleware

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ func main() {
 	router := router.NewRouter()
 
 	router.Use(middleware.Logger)
+	router.Use(middleware.RateLimiter)
+	router.Use(middleware.Throttle((100)))
+
 	router.Get("/transit", controllers.Transit())
 	router.Get("/autocomplete", controllers.Autocomplete)
 
